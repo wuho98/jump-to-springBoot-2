9d8d1a79668b3950615eb678e3e7fd2a416c0062
wuho98 <dngh112@gmail.com>
2026-03-12 16:22:58 +0900

    점프 투 스프링 부트 시작

---
+++ src/main/java/com/back/jumptospringboot/JumpToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.back.jumptospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpToSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumpToSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/jumptospringboot/JumpToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.jumptospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpToSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
